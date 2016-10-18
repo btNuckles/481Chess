6713891b350f1d9cde6b7990405af199ecab7e49
Kevin <vuongkevin1@gmail.com>
2016-10-18 01:59:38 -0700

    Added a function to create states of current board using valid the moves of current player. Note: Function still needs to alternate between players to create ply states.

--- 481Chess/PythonChessMain.py
+++ 481Chess/PythonChessMain.py
@@ -3,10 +3,10 @@
  Project: Python Chess
  File name: PythonChessMain.py
  Description:  Chess for player vs. player, player vs. AI, or AI vs. AI.
-	Uses Tkinter to get initial game parameters.  Uses Pygame to draw the 
-	board and pieces and to get user mouse clicks.  Run with the "-h" option 
-	to get full listing of available command line flags.  
-	
+        Uses Tkinter to get initial game parameters.  Uses Pygame to draw the 
+        board and pieces and to get user mouse clicks.  Run with the "-h" option 
+        to get full listing of available command line flags.  
+        
  Copyright (C) 2009 Steve Osborne, srosborne (at) gmail.com
  http://yakinikuman.wordpress.com/
  *******
@@ -76,124 +76,149 @@ import copy
 
 #Node for state tree, used to build the state tree
 class Tree(object):
-        def __init__(self, root):
-                self.root = copy.deepcopy(root) #stores copy of the board
+        def __init__(self, board):
+                self.board = copy.deepcopy(board) #stores copy of the ChessBoard
                 self.hVal = 0 #heuristic value
                 self.children = [] #Stores "tree objects," which are the nodes
                 
         def add_child(self, node):
                 assert isinstance(node,Tree) #Checks if the node object is of class Tree
                 self.children.append(node)
-
+                
+        def create_tree(self,color,rules):
+                board = self.board.GetState() #Gets the current state of chess board
+                if color == 'white':
+                        player = 'w'
+                if color == 'black':
+                        player = 'b'
+                for r in range(8):
+                        for c in range(8):
+                                if board[r][c] != 'e':
+                                        piece = board[r][c]
+                                        if piece[:1] == player: #If the piece is the current play's piece, create the children of this node
+                                                tup = (r,c) #Gets position of piece
+                                                mylist = rules.GetListOfValidMoves(board,color,tup) #Gets list of valid move for that piece
+                                                #This loops through the list of valid moves and makes the move on a temporary ChessBoard.
+                                                #Uses the temporary ChessBoard to create a child, and then appends the child to the children list.
+                                                #Each child contains the state of the board after a valid move has been made.
+                                                for moves in mylist:
+                                                        tempChessBoard = copy.deepcopy(self.board)
+                                                        tempChessBoard.MovePiece((tup,moves))
+                                                        tempTreeObj = Tree(tempChessBoard)
+                                                        self.add_child(tempTreeObj)
+                                                
 class PythonChessMain:
-	def __init__(self,options):
-		if options.debug:
-			self.Board = ChessBoard(2)
-			self.debugMode = True
-		else:
-			self.Board = ChessBoard(0)#0 for normal board setup; see ChessBoard class for other options (for testing purposes)
-			self.debugMode = False
+        def __init__(self,options):
+                if options.debug:
+                        self.Board = ChessBoard(2)
+                        self.debugMode = True
+                else:
+                        self.Board = ChessBoard(0)#0 for normal board setup; see ChessBoard class for other options (for testing purposes)
+                        self.debugMode = False
 
-		self.Rules = ChessRules()
-		
-	def SetUp(self,options):
-		#gameSetupParams: Player 1 and 2 Name, Color, Human/AI level
-		if self.debugMode:
-			player1Name = 'Kasparov'
-			player1Type = 'human'
-			player1Color = 'white'
-			player2Name = 'Light Blue'
-			player2Type = 'randomAI'
-			player2Color = 'black'		
-		else:
-			GameParams = TkinterGameSetupParams()
-			(player1Name, player1Color, player1Type, player2Name, player2Color, player2Type) = GameParams.GetGameSetupParams()
+                self.Rules = ChessRules()
+                
+        def SetUp(self,options):
+                #gameSetupParams: Player 1 and 2 Name, Color, Human/AI level
+                if self.debugMode:
+                        player1Name = 'Kasparov'
+                        player1Type = 'human'
+                        player1Color = 'white'
+                        player2Name = 'Light Blue'
+                        player2Type = 'randomAI'
+                        player2Color = 'black'          
+                else:
+                        GameParams = TkinterGameSetupParams()
+                        (player1Name, player1Color, player1Type, player2Name, player2Color, player2Type) = GameParams.GetGameSetupParams()
 
-		self.player = [0,0]
-		if player1Type == 'human':
-			self.player[0] = ChessPlayer(player1Name,player1Color)
-		elif player1Type == 'randomAI':
-			self.player[0] = ChessAI_random(player1Name,player1Color)
-		elif player1Type == 'defenseAI':
-			self.player[0] = ChessAI_defense(player1Name,player1Color)
-		elif player1Type == 'offenseAI':
-			self.player[0] = ChessAI_offense(player1Name,player1Color)
-			
-		if player2Type == 'human':
-			self.player[1] = ChessPlayer(player2Name,player2Color)
-		elif player2Type == 'randomAI':
-			self.player[1] = ChessAI_random(player2Name,player2Color)
-		elif player2Type == 'defenseAI':
-			self.player[1] = ChessAI_defense(player2Name,player2Color)
-		elif player2Type == 'offenseAI':
-			self.player[1] = ChessAI_offense(player2Name,player2Color)
-			
-		if 'AI' in self.player[0].GetType() and 'AI' in self.player[1].GetType():
-			self.AIvsAI = True
-		else:
-			self.AIvsAI = False
-			
-		if options.pauseSeconds > 0:
-			self.AIpause = True
-			self.AIpauseSeconds = int(options.pauseSeconds)
-		else:
-			self.AIpause = False
-			
-		#create the gui object - didn't do earlier because pygame conflicts with any gui manager (Tkinter, WxPython...)
-		if options.text:
-			self.guitype = 'text'
-			self.Gui = ChessGUI_text()
-		else:
-			self.guitype = 'pygame'
-			if options.old:
-				self.Gui = ChessGUI_pygame(0)
-			else:
-				self.Gui = ChessGUI_pygame(1)
-			
-	def MainLoop(self):
-		currentPlayerIndex = 0
-		turnCount = 0
-		while not self.Rules.IsCheckmate(self.Board.GetState(),self.player[currentPlayerIndex].color):
-			board = self.Board.GetState()
-			currentColor = self.player[currentPlayerIndex].GetColor()
-			#hardcoded so that player 1 is always white
-			if currentColor == 'white':
-				turnCount = turnCount + 1
-			if turnCount == 101:
-				self.Gui.PrintMessage("Maximum moves reached.")
-				self.Gui.EndGame(board)
-			self.Gui.PrintMessage("")
-			baseMsg = "TURN %s - %s (%s)" % (str(turnCount),self.player[currentPlayerIndex].GetName(),currentColor)
-			self.Gui.PrintMessage("-----%s-----" % baseMsg)
-			self.Gui.Draw(board)
-			if self.Rules.IsInCheck(board,currentColor):
-				self.Gui.PrintMessage("Warning..."+self.player[currentPlayerIndex].GetName()+" ("+self.player[currentPlayerIndex].GetColor()+") is in check!")
-			
-			if self.player[currentPlayerIndex].GetType() == 'AI':
-				moveTuple = self.player[currentPlayerIndex].GetMove(self.Board.GetState(), currentColor) 
-			else:
-				moveTuple = self.Gui.GetPlayerInput(board,currentColor)
-			moveReport = self.Board.MovePiece(moveTuple) #moveReport = string like "White Bishop moves from A1 to C3" (+) "and captures ___!"
-			self.Gui.PrintMessage(moveReport)
-			currentPlayerIndex = (currentPlayerIndex+1)%2 #this will cause the currentPlayerIndex to toggle between 1 and 0
-			if self.AIvsAI and self.AIpause:
-				time.sleep(self.AIpauseSeconds)
-		
-		self.Gui.PrintMessage("CHECKMATE!")
-		winnerIndex = (currentPlayerIndex+1)%2
-		self.Gui.PrintMessage(self.player[winnerIndex].GetName()+" ("+self.player[winnerIndex].GetColor()+") won the game!")
-		self.Gui.EndGame(board)
-		
+                self.player = [0,0]
+                if player1Type == 'human':
+                        self.player[0] = ChessPlayer(player1Name,player1Color)
+                elif player1Type == 'randomAI':
+                        self.player[0] = ChessAI_random(player1Name,player1Color)
+                elif player1Type == 'defenseAI':
+                        self.player[0] = ChessAI_defense(player1Name,player1Color)
+                elif player1Type == 'offenseAI':
+                        self.player[0] = ChessAI_offense(player1Name,player1Color)
+                        
+                if player2Type == 'human':
+                        self.player[1] = ChessPlayer(player2Name,player2Color)
+                elif player2Type == 'randomAI':
+                        self.player[1] = ChessAI_random(player2Name,player2Color)
+                elif player2Type == 'defenseAI':
+                        self.player[1] = ChessAI_defense(player2Name,player2Color)
+                elif player2Type == 'offenseAI':
+                        self.player[1] = ChessAI_offense(player2Name,player2Color)
+                        
+                if 'AI' in self.player[0].GetType() and 'AI' in self.player[1].GetType():
+                        self.AIvsAI = True
+                else:
+                        self.AIvsAI = False
+                        
+                if options.pauseSeconds > 0:
+                        self.AIpause = True
+                        self.AIpauseSeconds = int(options.pauseSeconds)
+                else:
+                        self.AIpause = False
+                        
+                #create the gui object - didn't do earlier because pygame conflicts with any gui manager (Tkinter, WxPython...)
+                if options.text:
+                        self.guitype = 'text'
+                        self.Gui = ChessGUI_text()
+                else:
+                        self.guitype = 'pygame'
+                        if options.old:
+                                self.Gui = ChessGUI_pygame(0)
+                        else:
+                                self.Gui = ChessGUI_pygame(1)
+                        
+        def MainLoop(self):
+                currentPlayerIndex = 0
+                turnCount = 0
+                while not self.Rules.IsCheckmate(self.Board.GetState(),self.player[currentPlayerIndex].color):
+                        board = self.Board.GetState()
+                        currentColor = self.player[currentPlayerIndex].GetColor()
+                        #Create game state tree (kevin)
+                        gameStateTree = Tree(self.Board)
+                        gameStateTree.create_tree(currentColor,self.Rules)
+                        #hardcoded so that player 1 is always white
+                        if currentColor == 'white':
+                                turnCount = turnCount + 1
+                        if turnCount == 101:
+                                self.Gui.PrintMessage("Maximum moves reached.")
+                                self.Gui.EndGame(board)
+                        self.Gui.PrintMessage("")
+                        baseMsg = "TURN %s - %s (%s)" % (str(turnCount),self.player[currentPlayerIndex].GetName(),currentColor)
+                        self.Gui.PrintMessage("-----%s-----" % baseMsg)
+                        self.Gui.Draw(board)
+                        if self.Rules.IsInCheck(board,currentColor):
+                                self.Gui.PrintMessage("Warning..."+self.player[currentPlayerIndex].GetName()+" ("+self.player[currentPlayerIndex].GetColor()+") is in check!")
+                        
+                        if self.player[currentPlayerIndex].GetType() == 'AI':
+                                moveTuple = self.player[currentPlayerIndex].GetMove(self.Board.GetState(), currentColor) 
+                        else:
+                                moveTuple = self.Gui.GetPlayerInput(board,currentColor)
+                        moveReport = self.Board.MovePiece(moveTuple) #moveReport = string like "White Bishop moves from A1 to C3" (+) "and captures ___!"
+                        self.Gui.PrintMessage(moveReport)
+                        currentPlayerIndex = (currentPlayerIndex+1)%2 #this will cause the currentPlayerIndex to toggle between 1 and 0
+                        if self.AIvsAI and self.AIpause:
+                                time.sleep(self.AIpauseSeconds)
+                
+                self.Gui.PrintMessage("CHECKMATE!")
+                winnerIndex = (currentPlayerIndex+1)%2
+                self.Gui.PrintMessage(self.player[winnerIndex].GetName()+" ("+self.player[winnerIndex].GetColor()+") won the game!")
+                self.Gui.EndGame(board)
+                
 
 parser = OptionParser()
 parser.add_option("-d", dest="debug",
-				  action="store_true", default=False, help="Enable debug mode (different starting board configuration)")
+                                  action="store_true", default=False, help="Enable debug mode (different starting board configuration)")
 parser.add_option("-t", dest="text",
-				  action="store_true", default=False, help="Use text-based GUI")
+                                  action="store_true", default=False, help="Use text-based GUI")
 parser.add_option("-o", dest="old",
-				  action="store_true", default=False, help="Use old graphics in pygame GUI")
+                                  action="store_true", default=False, help="Use old graphics in pygame GUI")
 parser.add_option("-p", dest="pauseSeconds", metavar="SECONDS",
-				  action="store", default=0, help="Sets time to pause between moves in AI vs. AI games (default = 0)")
+                                  action="store", default=0, help="Sets time to pause between moves in AI vs. AI games (default = 0)")
 
 
 (options,args) = parser.parse_args()
@@ -203,4 +228,4 @@ game.SetUp(options)
 game.MainLoop()
 
 
-	
+        
